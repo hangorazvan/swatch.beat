6988ac3eeefe48e410f62f4dae8a0185c0bcc51d
hangorazvan <58172000+hangorazvan@users.noreply.github.com>
2020-02-12 22:12:27 +0200

    Add files via upload

---
+++ MMM-swatch/MMM-swatch.js
@@ -0,0 +1,29 @@
+Module.register("MMM-swatch", {
+
+	defaults: {
+		beat: '<img class="logo" src="MMM-swatch.svg"></img>&nbsp; <span class="beat"></span>&nbsp; <img class="logo" src="MMM-beat.svg"></img>',
+	},
+	
+	start: function() {
+        Log.info("Starting module: " + this.name);
+	},
+	
+	getStyles: function() {
+		return ["MMM-swatch.css"];
+	},
+
+	getDom: function() {
+		function swatch(){
+            var d=new Date();
+            var h=d.getUTCHours()+1;
+            var m=d.getUTCMinutes();
+            var s=d.getUTCSeconds();
+            var time='@'+('000'+Math.floor((h*3600+m*60+s)/86.4)%1000).slice(-3);
+            $('.beat').html(time);
+        } setInterval(swatch,864);
+		var wrapper = document.createElement("div");
+		wrapper.innerHTML = this.config.beat;
+		wrapper.className = "swatch";
+		return wrapper;
+	}
+});
