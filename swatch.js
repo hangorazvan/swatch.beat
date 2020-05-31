e3b6d7275774f24d530ea98344e7421c831b4686
Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2020-05-31 19:21:22 +0300

    Add files via upload

--- swatch.js
+++ swatch.js
@@ -22,7 +22,7 @@ Module.register("swatch", {
 			var h = d.getUTCHours() + 1;
 			var m = d.getUTCMinutes();
 			var s = d.getUTCSeconds();
-			var time = "@" + ("000" + Math.floor((h * 3600 + m * 60 + s) / 86.4) / 1000).slice(-3);
+			var time = "@" + ("." + Math.floor((h * 3600 + m * 60 + s) / 86.4) / 1000).slice(-3);
 			document.getElementsByClassName("beat")[0].innerHTML = time;
 		} setInterval(swatch, 864);
 		var wrapper = document.createElement("div");
