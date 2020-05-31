74db8855329b1284aba7c217ef35c4e71d5fff71
Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2020-05-31 19:16:06 +0300

    Add files via upload

--- swatch.js
+++ swatch.js
@@ -1,34 +1,32 @@
-/* Magic Mirror 2
-*  Module: Swatch Internet Time
-*  by Razvan Cristea 
-*  https://github.com/hangorazvan
+/*	Magic Mirror 2
+*	Module: Swatch
+*	by Razvan Cristea 
+*	https://github.com/hangorazvan
 */
 
 Module.register("swatch", {
 
-	defaults: {
-		beat: '<img class="logo" src="swatch.svg"></img>&nbsp; <span class="beat"></span>&nbsp; <img class="logo" src="beat.svg"></img>',
-	},
+	defaults: {},
 	
 	start: function() {
-        Log.info("Starting module: " + this.name);
+		Log.info("Starting module: " + this.name);
 	},
 	
-	getScripts: function() {return ["moment.js", "jquery.min.js"];},
-	
-	getStyles: function() {return ["swatch.css"];},
+	getStyles: function() {
+		return 0;
+	},
 
 	getDom: function() {
 		function swatch(){
-            var d=new Date();
-            var h=d.getUTCHours()+1;
-            var m=d.getUTCMinutes();
-            var s=d.getUTCSeconds();
-            var time="@"+("000"+Math.floor((h*3600+m*60+s)/86.4)%1000).slice(3);
-            $(".beat").html(time);
-        } setInterval(swatch,864);
+			var d = new Date();
+			var h = d.getUTCHours() + 1;
+			var m = d.getUTCMinutes();
+			var s = d.getUTCSeconds();
+			var time = "@" + ("000" + Math.floor((h * 3600 + m * 60 + s) / 86.4) / 1000).slice(-3);
+			document.getElementsByClassName("beat")[0].innerHTML = time;
+		} setInterval(swatch, 864);
 		var wrapper = document.createElement("div");
-		wrapper.innerHTML = this.config.internetTime;
+		wrapper.innerHTML = this.config.beat;
 		wrapper.className = "swatch";
 		return wrapper;
 	}
